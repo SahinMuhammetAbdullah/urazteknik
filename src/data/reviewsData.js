2524bba648db0d479a2a9e19173f7aba5cd43699
SahinMuhammetAbdullah <m.sahingithub@gmail.com>
2025-07-09 21:23:59 +0300

    main

--- src/data/reviewsData.js
+++ src/data/reviewsData.js
@@ -5,13 +5,28 @@ export const reviewsData = [
         stars: 5
     },
     {
-        name: "Ali AKSU",
-        text: "Uraz teknik firmasından hizmet aldım profesyonel ve güleryüzlü bir ekibi var sorunumu hızlıca çözdüler herkese tavsiye ederim",
+        name: "Uğur yücel Aksu",
+        text: "Kombim arızalanmıştı 4 kere ayrı servisler geldi çözemedi uraz teknikten mert bey saolsun arızayı bulup tamir etti ilginiz için teşekkür ederim🙏 …",
         stars: 5
     },
     {
-        name: "Canan Bayram",
-        text: "Hızlı ve güvenilir işçilik tavsiye ederim",
+        name: "Serhat Çam",
+        text: "Kaliteli ve güvenilir işçilik servisin su gider borusu dışarıya gitmez dediği için evimin içinde kovaya yada pet şişeye akan suyu 5 dakika da hallettiler herkese tavsiye ederim",
+        stars: 5
+    },
+    {
+        name: "yaşar sefa akakça",
+        text: "uraz teknik firmasını aradım yarım saat bile geçmeden hizmet aldım arkadaşlar yeni firma açtıklarını bu yüzden fiyatların uygun olduğunu söylediler gerçekten de fiyatları çok uygun",
+        stars: 5
+    },
+    {
+        name: "Nurcan Yılmaz",
+        text: "Aldığım hizmetten çok memnun kaldım kısa sürede gelip kombimin sorununu çözdüler",
+        stars: 5
+    },
+    {
+        name: "Eren Mırık",
+        text: "Başarılı bir servis herkese tavsiye ediyorum",
         stars: 5
     }
 ];
