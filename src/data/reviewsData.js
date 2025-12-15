d4d15daefb0834f10d9ac141b0c04167011ab12f
SahinMuhammetAbdullah <m.sahingithub@gmail.com>
2025-12-15 20:03:09 +0300

    main

--- src/data/reviewsData.js
+++ src/data/reviewsData.js
@@ -43,6 +43,66 @@ export const allReviews  = [
         name: "ibrahim şerefli",
         text: "İş yerimde elimde bulan bir klimanın kurulumunu için bir kaç kişi ile görüştüm. Uraz Teknikten Mert beyle görüştükten sonra hizmetlerinin uygun olması ve sitelerinde yaptıkları işlerin fotoğraflarını gördükten sonra kendisi ile çalışmaya karar verdim. İşini seven ve tecrübeli birisi. Mert Beye teşekkür ederim.",
         stars: 5
+    },
+    {
+        name: "Toprak Küçük",
+        text: "Kombi bakımı ve petek temizliği yaptırdım kaliteli işçilik uygun fiyat düşünmeden arayabilirsiniz",
+        stars: 5
+    },
+    {
+        name: "Artek Medikal",
+        text: "Adamlar işinin Ehl-i kesinlikle tavsiye ederim",
+        stars: 5
+    },
+    {
+        name: "Sevilay ATEŞ",
+        text: "Kombi bakimi ve petek temizligi yaptirdim,gayet guzel ve düzenli calisiyorlar,ben memnun kaldim",
+        stars: 5
+    },
+    {
+        name: "Barış Kara",
+        text: "kombi ve petek temizliği işlemi yaptırdım tecrğbeli ve güvenilir işçilik herkese tavsiye ederim",
+        stars: 5
+    },
+    {
+        name: "Kiymet Yildrim",
+        text: "Kombi bakimi yaptırdım aradım yarım saat bile sürmeden  geldiler düşünmeden arayabilirsiniz",
+        stars: 5
+    },
+    {
+        name: "Esat Yıldırım",
+        text: "Tecrübeli bir işçilik kombimin bakımını yaptırdım memnun kaldım tavsiye ederim",
+        stars: 5
+    },
+    {
+        name: "Berkay Özatılgan",
+        text: "3 gün önce klima bakımında gelmişlerdi çok memnun kaldım gerçekten kaliteli işçilik ıstıyorsanız bir deneyin",
+        stars: 5
+    },
+    {
+        name: "Kerem Kara",
+        text: "Çok memnun kaldım herkese tavsiye ederim temiz işçilik ve güler yüzlü hizmet verdiler bundan sonra tek adresim URAZ TEKNİK",
+        stars: 5
+    },
+    {
+        name: "Ayşegül Yüksel",
+        text: "Kombi bakımı yaptırdım gayet memnun kaldım detaylı bir şekilde bakımını yaptı çok teşekkür ederim mert bey’e  işinde baya tecrübeli birisi",
+        stars: 5
+    },
+    {
+        name: "Talha Dikmen",
+        text: "Kombime bakım yapıldı peteklerim temizlendi tecrübeli bir işçilik hiç biryer kirlenmedi çok memnun kaldım",
+        stars: 5
+    },
+    {
+        name: "Turgay Bayram",
+        text: "Cihazımın arızası vardı uraz teknikten Mert bey hızlı bir şekilde adresime ulaştı ve arızamı uygun fiyata giderdi teşekkür ederim",
+        stars: 5
+    },
+    {
+        name: "Can Ahmet Yigman",
+        text: "İşinde gayet iyi ve ilgili uzman gönül rahatlığıyla arayabilir ve çağırabilirsiniz mert bey ve ekibine teşekkür ederim 🙏🙃 …",
+        stars: 5
     }
 ];
 
@@ -64,7 +124,7 @@ function shuffleArray(array) {
   
   // Ana sayfada gösterilecek yorumları dinamik olarak oluştur
   // Önce tüm yorumları karıştır, sonra ilk üçünü al
-  export const mainPageReviews = shuffleArray(allReviews).slice(0, 3);
+  export const mainPageReviews = shuffleArray(allReviews).slice(0, 6);
   
   // Yorumlar sayfasında tüm yorumları göstermek için (sıralı)
   export const allReviewsData = allReviews;
