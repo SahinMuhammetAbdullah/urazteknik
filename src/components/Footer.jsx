b4bbe34bad571e02196e6e536ac53f48fc21c1f6
SahinMuhammetAbdullah <m.sahingithub@gmail.com>
2026-05-19 23:35:10 +0300

    main

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 
-// Scroll işlemini gerçekleştiren fonksiyon
 const scrollToTop = () => {
   window.scrollTo(0, 0);
 };
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer>
       <div className="container">
@@ -18,12 +18,9 @@ function Footer() {
           <div className="footer-col">
             <h3 className="footer-title">Site Haritası</h3>
             <ul className="footer-links">
-              {/* HashLink'ler zaten sayfa içi kaydırma yapar, onlara dokunmuyoruz */}
               <li><HashLink smooth to="/#anasayfa">Ana Sayfa</HashLink></li>
               <li><HashLink smooth to="/#hizmetler">Hizmetlerimiz</HashLink></li>
               <li><HashLink smooth to="/#yorumlar">Müşteri Yorumları</HashLink></li>
-              
-              {/* Yeni sayfaya giden Link'lere onClick ile kaydırma eklendi */}
               <li><Link to="/iletisim" onClick={scrollToTop}>İletişim</Link></li>
               <li><Link to="/galeri" onClick={scrollToTop}>Galeri</Link></li>
             </ul>
@@ -31,7 +28,6 @@ function Footer() {
           <div className="footer-col">
             <h3 className="footer-title">Hizmet Bölgelerimiz</h3>
             <ul className="footer-links">
-              {/* Yeni sayfaya giden Link'lere onClick ile kaydırma eklendi */}
               <li><Link to="/hizmet/atakum-kombi-klima-servisi" onClick={scrollToTop}>Atakum Kombi ve Klima Servisi</Link></li>
               <li><Link to="/hizmet/ilkadim-kombi-klima-servisi" onClick={scrollToTop}>İlkadım Kombi ve Klima Servisi</Link></li>
               <li><Link to="/hizmet/canik-kombi-klima-servisi" onClick={scrollToTop}>Canik Kombi ve Klima Servisi</Link></li>
@@ -40,7 +36,7 @@ function Footer() {
           </div>
         </div>
         <div className="footer-bottom">
-          <p>© 2025 Uraz Teknik Isınma ve Soğutma Servisi. Tüm hakları saklıdır. | 7/24 Hizmet Hattı: 0543 206 40 55</p>
+          <p>© {currentYear} Uraz Teknik Isınma ve Soğutma Servisi. Tüm hakları saklıdır. | 7/24 Hizmet Hattı: 0543 206 40 55</p>
         </div>
       </div>
     </footer>
